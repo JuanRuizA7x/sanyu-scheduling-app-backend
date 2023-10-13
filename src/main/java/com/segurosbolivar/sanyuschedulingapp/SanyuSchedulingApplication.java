7708f31a6b614754a34c4941b98637b75b2d8b69
Juan Ruiz <juan.ruiz.22.03.00@gmail.com>
2023-10-13 11:52:10 -0500

    Configuración inicial del proyecto

---
+++ src/main/java/com/segurosbolivar/sanyuschedulingapp/SanyuSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.segurosbolivar.sanyuschedulingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanyuSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanyuSchedulingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/segurosbolivar/sanyuschedulingapp/SanyuSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segurosbolivar.sanyuschedulingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanyuSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
