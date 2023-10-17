ee4033c8e314009ede88f3cfa70fa7dcc2e99b3d
Juan Ruiz <juan.ruiz.22.03.00@gmail.com>
2023-10-17 09:53:10 -0500

    Configuración inicial de Swagger

---
+++ src/main/java/com/segurosbolivar/sanyuschedulingapp/configuration/SwaggerConfiguration.java
@@ -0,0 +1,25 @@
+package com.segurosbolivar.sanyuschedulingapp.configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("SANYU Scheduling App")
+                        .description("Application for the management of work shifts of the contractors of the SANYU company")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
+    }
+
+}
